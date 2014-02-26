58cd73f5db584e030987865c35fe955c2003fc92
Strongme <624917867@qq.com>
2014-02-26 16:09:51 +0800

    First commit

---
+++ js/strongg.js
@@ -0,0 +1,17 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: walter
+ * Date: 14-2-26
+ * Time: 下午3:56
+ * To change this template use File | Settings | File Templates.
+ */
+
+var StrongG = (function(module) {
+    module.doIt = function(targetObj) {
+        targetObj.prepend('<a href="https://github.com/strongme">Mail Me</a>');
+    }
+    module.clear = function(targetObj) {
+        targetObj.html('');
+    }
+    return module;
+})(StrongG || {});
